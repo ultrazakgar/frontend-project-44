6fd5c22bd6f20bf0e68a32d1bd22bfa68042119b
ultrazakgar <zahariavinogr@gmail.com>
2026-02-07 01:30:53 +0300

    step 8

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,16 +3,6 @@
  * run game with arguments/ defaut - from node command line
  * @example `node bin/brain-games.js` 
  */
-const args = process.argv.slice(2);
-import {playGame} from '../games/playGame.js';
+import {doit} from '../games/playGame.js';
 
-let results = {userName :'anonymous'};
-
-await playGame('user',results);
-results.userName = results['user'][0];
-await playGame('hello',results);
-
-if (!!(args[0])) { // запустили не из экспортной версии?
-     playGame(args[0]);
-} 
-
+doit('');
