781c52fc8f765c4963aaa0665dbe86f6e9ce90b0
ultrazakgar <zahariavinogr@gmail.com>
2026-02-06 23:53:01 +0300

    step 2

--- games/parentGame.js
+++ games/parentGame.js
@@ -8,38 +8,7 @@ class parentGame {
         return min + Math.floor(Math.random() * (max - min));
     }
 
-    playGame(game_name,userName) {
-        return import(`../games/${game_name}Module.js`)
-            .then(module => {
-                let game = module.default
-                let rounds = game.rounds || 3;
-                results[game_name] = [];
-                if (!!game.description)
-                    console.log(game.description || 'Game have no decription, just do what you want');
-                // game=game.default.get();
-                for (let i = 0; i < rounds; i++) {
-                    let [question, mark, correct] = game.play(i,userName), userAnswer;
-                    if(!!question)
-                        console.log(`${question}`);
-                    if(null !== mark) {
-                        userAnswer = readlineSync.question(`${mark}`);
-                        results[game_name].push(userAnswer);
-                    }
-                    if (correct !== null) {
-                        if (userAnswer === correct.toString().toLowerCase()) {
-                            console.log('Correct!');
-                        } else {
-                            if (i + 1 === rounds) {
-                                console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correct}'.`);
-                            } else {
-                                console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correct}'.\nLet's try again, ${userName}!`);
-                            }
-                        }
-                    }
-                }
-            }).catch(err => console.log(`Sorry, game ${args[0]} not found.`));
-    }
-    
+     
 }
 
 export default parentGame
